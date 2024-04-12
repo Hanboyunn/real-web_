08210b88483514cf64da31f0c748aac6c2504764
Hanboyunn <154271682+Hanboyunn@users.noreply.github.com>
2024-04-12 17:20:30 +0900

    1

---
+++ app/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+user_name = None
+
+class User(BaseModel):
+    name: str
+
+
+@app.get("/")
+def root():
+    return{ "message": "Hello Bobo!"}
+
+@app.get("/home")
+def home():
+    return { "message": "Bye bobo ㅠㅠ!" }
+
+#####여기까지 저번주########
+
+
+@app.post("/user/")
+async def receive_user(user: User):
+    global user_name
+    user_name = user.name
+    return {"message": "User name received"}
+
+
+@app.get("/user/")
+async def get_user():
+    return {"user_name": user_name}
+
+@app.put("/user/")
+async def receive_user(user: User):
+    global user_name
+    user_name = user.name
+    return {"message": "User name changed"}
+
+
+@app.delete("/user/")
+async def del_user():
+    global user_name
+    user_name = "DELETED"
+    return {"message": "User name deleted"}
+
+
+######
+
+# 클래스 쓰는 법
+
+user1=User(name="bobo hihi")
+
+
+
+###########
